c27c2d26d145109d20a58d323e2262ac03445ed4
Snorrenv <snorre@vestli.info>
2011-02-22 14:20:50 +0100

    slettet build-mappen, da den krasjer make.

--- xplain-software-framework-1.0/source/build/display-training/xplain/GNU/arch/avr8/.link.lds.d
+++
@@ -1,6 +0,0 @@
-arch/avr8/link.lds: ../../../../arch/avr8/link.lds.S include/autoconf.h \
-  ../../../../chip/atxmega128a1/include/chip/memory-map.h
-
-include/autoconf.h:
-
-../../../../chip/atxmega128a1/include/chip/memory-map.h:
