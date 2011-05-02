124685664f54b86e3bdf013cae1d5ad873196af8
Daniel Georg Weber <danielgw@stud.hist.no>
2011-05-02 09:43:02 +0200

    doxy fix

--- xplain-software-framework-1.0/source/apps/gauge-demo/main.c
+++ xplain-software-framework-1.0/source/apps/gauge-demo/main.c
@@ -44,13 +44,15 @@
  * the gauge widget. The application has two different sliders which input 
  * values for the two separate gauges. It also demonstrates the use of some of 
  * the implemented options for the gauge.
- *
+ * 
+ * The documentation of the the demo application is found here: \ref apps_demo_group "Demo applications"
+ * 
+ *  This demo is also based upon the following modules:
  * - \ref gfx "Graphics display system"
  * - \ref gfx_gfx "Graphics drivers"
  * - \ref gfx_win "Window system"
  * - \ref gfx_wtk "Widget toolkit"
  * - \ref touch_driver_group "Touch drivers"
- * - \ref apps_demo_group "Demo applications"
  */
 
 /**
